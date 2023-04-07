f1026da8de6aefa09daa4a69f38024cb7367006f
arny-wilk <arnywilk@gmail.com>
2023-04-07 14:39:38 +0200

    Bootstrap project

---
+++ demo/src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    void testMain() {
+        assertTrue("Hello World!", true);
+    }
+}
